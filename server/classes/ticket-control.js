5cbdd374db31ba5a409cf72a8d64717b96d31a0a
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-29 14:58:29 -0300

    Add arquivos extras ao projeot

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -10,15 +10,35 @@ class TicketControl{
         let data = require('../data/data.json');
         //Compara a data atual em data.json
         if(data.hoje === this.hoje){
-
+            //Se a data for igual a de hoje pegar o número do ultimo ticket
+            this.ultimo = data.ultimo;
         }else{
             //Se a data for diferente da data de hoje reinicia o ticket
             this.reiniciarContador();
         }      
     }
 
+    proximoTicket(){
+        //Incrementando o contador de tickets
+        this.ultimo += 1;
+        //Grava as alterações de ticket no DB
+        this.gravarArquivo();
+        //Retorna o ultimo ticket para mostrar na Tela
+        return `Ticket ${this.ultimo}`;
+    }
+
     //Reinicia o contador de Tickets
     reiniciarContador(){
+        //Zera a variável ultimo
+        this.ultimo = 0;
+
+        console.log('O sistema foi reinicializado');
+        
+        //Grava as alterações de data e ticket no DB
+        this.gravarArquivo();
+    }
+
+    gravarArquivo(){
         let jsonData = {
             ultimo: this.ultimo = 0,
             hoje: this.hoje
@@ -26,7 +46,7 @@ class TicketControl{
         //Mandando como string json
         let jsonDataString = JSON.stringify(jsonData); 
         //Gravando a informação no data.json 
-        fs.writeFileSync('./server/data/data.json', jsonDataString);    
+        fs.writeFileSync('./server/data/data.json', jsonDataString); 
     }
 }
 
