1e88d486964c9cc994d6d379a7e036ade42a186b
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-29 15:12:08 -0300

    Definindo um listener no button do frontend para gerar o novo ticket

--- public/js/socket.novo-ticket.js
+++ public/js/socket.novo-ticket.js
@@ -8,3 +8,9 @@ socket.on('connect', function(){
 socket.on('disconnect', function(){
     console.log('Desconectado do Servidor');
 });
+
+//Definindo que todos os buttons da tela executem esta função
+$('button').on('click', function(){
+    console.log('click');
+});
+//Clique no botão da tela novo-ticket.html e veja o resultado no console
