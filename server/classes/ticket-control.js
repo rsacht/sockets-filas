a637f4f6271f7a500b7c3a2e1a89cdfe3976b4b2
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-29 13:50:17 -0300

    Gravando dados no data.json e reiniciando o sistema de tickets caso a data em data.json seja diferente da data de hoje

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -1,3 +1,5 @@
+//Importando file system para podermos salvar os dados no data.json
+const fs = require('fs');
 
 class TicketControl{
     constructor(){
@@ -6,7 +8,25 @@ class TicketControl{
 
         //Lendo o arquivo de dados data.json
         let data = require('../data/data.json');
-        console.log(data);
+        //Compara a data atual em data.json
+        if(data.hoje === this.hoje){
+
+        }else{
+            //Se a data for diferente da data de hoje reinicia o ticket
+            this.reiniciarContador();
+        }      
+    }
+
+    //Reinicia o contador de Tickets
+    reiniciarContador(){
+        let jsonData = {
+            ultimo: this.ultimo = 0,
+            hoje: this.hoje
+        }; 
+        //Mandando como string json
+        let jsonDataString = JSON.stringify(jsonData); 
+        //Gravando a informação no data.json 
+        fs.writeFileSync('./server/data/data.json', jsonDataString);    
     }
 }
 
